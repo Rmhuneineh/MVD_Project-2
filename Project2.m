5d62687b7c28e124c44f1ad0d5c9690fc96858d0
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-11-04 18:21:44 +0100

    Finished. Needs Revision

--- Project2.m
+++ Project2.m
@@ -54,7 +54,7 @@ ylabel('uyp [-]');
 
 figure; hold on;
 plot(gamma, BCDcamber);
-title('BCDcamber vs gamma');
+title('BCDcamber vs gamma at Fz=5kN');
 xlabel('gamma [deg]');
 ylabel('BCDcamber [-]');
 
@@ -76,7 +76,7 @@ ylabel('BCDalign [-]');
 
 figure; hold on;
 plot(gamma, BCDalignCamber);
-title('BCDalignCamber vs gamma');
+title('BCDalignCamber vs gamma at Fz=5kN');
 xlabel('gamma [deg]');
 ylabel('BCDalignCamber [-]');
 
@@ -113,7 +113,7 @@ end
 legend('ux [2kN]', 'ux [4kN]', 'ux [6kN]', 'ux [8kN]', 'ux [10kN]');
 xlabel('Slip [-]');
 ylabel('ux [-]');
-title('ux vs Slip');
+title('ux vs Slip for a range of Fz');
 
 % Evaluate Lateral Force Coefficient
 uy = zeros(201, 5);
@@ -131,9 +131,9 @@ end
 legend('uy [2kN]', 'uy [4kN]', 'uy [6kN]', 'uy [8kN]', 'uy [10kN]');
 xlabel('SideSlip [-]');
 ylabel('uy [-]');
-title('uy vs Slip');
+title('uy vs Slip for a range of Fz');
 
-% Calculate Lever Arm t[mm] at Fz=4kN
+% Calculate and Plot Lever Arm t[mm] at Fz=4kN
 t = Mz(index, 2)./Fy(index, 2)*10^3;
 
 figure; hold on;
@@ -141,3 +141,75 @@ plot(SideSlip, t);
 title('Lever Arm vs SideSlip at Fz=4kN');
 xlabel('SideSlip [-]');
 ylabel('t[mm]');
+
+% Plot Gough Diagram
+figure; hold on;
+for i = 1:5
+    plot(Mz(index, i), Fy(index, i));
+end
+
+for i = index
+   plot(Mz(i, 1:5), Fy(i, 1:5)); 
+end
+grid on;
+title('Fy vs Mz in ranges of Fz and alpha');
+xlabel('Mz [Nm]');
+ylabel('Fy [N]');
+
+% INTERACTION OF TIRE-ROAD FORCES IN LONGITUDINAL AND LATERAL DIRECTIONS
+% Evaluate and Plot Fy as a function of Fx at Fz = 4kN and Camber = 0 for different
+% side slip
+indeces = [111 121 131 141 151];
+Fy0 = Fy(indeces(1:5)); % [N]
+Fx0 = Fz(2)*uxp(2); % [N]
+Fyn = zeros(201, 5);
+for i = 1:5
+    for j = index
+        Fyn(j, i) = Fy0(i).*sqrt(1-(Fx(j, 2)/Fx0)^2); 
+    end
+end
+
+figure; hold on;
+for i = 1:5
+   plot(Fx(index, 2), Fyn(index, i)); 
+end
+legend('alpha = 2', 'alpha = 4', 'alpha = 6', 'alpha = 8', 'alpha = 10');
+title('Fy vs Fx for a range of alpha');
+xlabel('Fx [N]');
+ylabel('Fy [N]');
+
+% Evaluate and Plot Side Slip Stiffness as a function of Fx at different values of
+% Fz
+C0 = zeros(201, 5);
+C = zeros(201, 5);
+for i = 1:5
+   for j = index
+       if (Fx(j, i) < uxp(i)*Fz(i))
+           C(j, i) = BCDside(i)*sqrt(1-(Fx(j, i)/(uxp(i)*Fz(i)))^2);
+       else
+           C(j, i) = 0;
+       end
+   end
+end
+
+figure; hold on;
+for i = 1:5
+   plot(Fx(index, i), C(index, i)); 
+end
+legend('Fz = 2kN', 'Fz = 4kN', 'Fz = 6kN', 'Fz = 8kN', 'Fz = 10kN');
+title('C vs Fx for a range of Fz');
+xlabel('Fx [N]');
+ylabel('C [N/deg]');
+
+% Evaluate and Plot Lateral Force Coefficient uy as a function of ux at Fz=4kN
+uyn = zeros(201, 1);
+
+for i = index
+   uyn(i) = max(uy(index, 2))*sqrt(1-(ux(i, 2)/uxp(2))^2); 
+end
+
+figure; hold on;
+plot(ux(index, 2), uyn(index));
+title('uy vs ux at Fz = 4kN');
+xlabel('ux [-]');
+ylabel('uy [-]');
